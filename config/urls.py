d18d0874ef71549dd7f62626236398d9d86e7cd1
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-12-20 01:40:09 +0600

    updated users

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from config import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -31,3 +34,7 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
