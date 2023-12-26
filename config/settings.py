30e1c2accbf6703f247ab52522e4f9c55deb0f3b
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-12-26 23:56:00 +0600

    update settings file

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     
     # third party apps
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
